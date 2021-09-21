01d0a2081dfdb409fe992f512fd7236d90210990
Gokul K <gkattamanchi@gmail.com>
2021-09-21 12:30:43 -0400

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Gokul Kattamanchi on 9/21/21.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
